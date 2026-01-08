031542b3972e518050b390fc94888b3065b38e6f
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-01-08 13:02:29 +0100

    adding log in functionality

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+import Login from "./pages/Login";
+
+export const Home = () => {
+  return <div>Home</div>
+};
+
+
+function App() {
+  return(
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login/>}/>
+
+    </Routes>
+  );
+}
+
+export default App;
---
+++ frontend/src/lib/api.js
@@ -0,0 +1,1 @@
+export const login = async (data) => API.post("/auth/login", data);
